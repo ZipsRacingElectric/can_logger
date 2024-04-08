d796efb1797ce44a47686adce644bdc60d166525
ErickLe02 <ericklepers1@gmail.com>
2024-04-07 23:24:06 -0400

    Create reader for vcan0 socket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use socketcan::{CanSocket, Socket, frame::CanFrame};
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let mut _can_socket_tx = CanSocket::open("vcan0").unwrap();
+    
+    loop {
+        // TODO: Read the can frame data from can_read
+        let can_read = _can_socket_tx.read_frame().unwrap();
+        // let can_read_frame = CanFrame::Data((can_read.));
+
+        println!("{:?}", can_read);
+    }
+    
+
 }
